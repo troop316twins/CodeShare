94f05d1dbba9cf87af1aff128bf414c00043938c
troop316twins <joelmonza@gmail.com>
2016-01-27 12:51:51 -0500

    First Upload

---
+++ Main.py
@@ -0,0 +1,30 @@
+"""
+To do:
+Overall:
+    Output from Primavera
+            Note which should have been updated?  (Highlight what is in progress?)
+        Outputs to Procurement
+        Outputs to 310
+        Outputs to 330
+        E-mail outputs
+    Input to Primavera
+        NOT AUTOMATED?
+    Update lists
+        Move current outputs to last week
+        Print lists to file.
+    Update powerpoint
+        Save powerpoint to new date
+        Move old powerpoint to outdated file
+        E-mail procurement and ask for update
+        Output lists for graphs
+        Update Graphs
+        Update Powerpoint
+        E-mail Powerpoint (MANUAL)
+E-mail Program (excel file, what department to send to) -> email
+Output from Primavera(what document you want)-> excel named with that date
+Print out from Primavera(what document you want)-> PDF named with that date
+Update List () -> use output from Primavera
+Update Powerpoint() - > new powerpoint
+
+another line
+    """
