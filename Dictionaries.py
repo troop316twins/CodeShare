9833349c227b92ab58be2bb7d278f068896a7718
troop316twins <joelmonza@gmail.com>
2016-01-29 08:43:27 -0500

    First Upload

---
+++ Dictionaries.py
@@ -0,0 +1,9 @@
+"""
+Using the name of the layout returns the distance from the bottom of the list.
+"""
+PrimaveraLayouts = {'one': 1,
+           'two': 2,
+           'three': 3}
+EMail = {}
+Prepteam1 = {'jack': "{email:thisemail}"}
+Reports = {'EngineeringPrepTeams': "Jack,Chris Casey,Mike,Engineering"}
